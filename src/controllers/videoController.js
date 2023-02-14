acbfb07caba1d4a7032c8aa1cfb7272fe980c4b0
Msimusi <sihyun0708@gmail.com>
2023-02-14 20:24:16 +0900

    pug in progress

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,15 +1,6 @@
-export const trending = (req, res) => res.send("Home Page Videos");
-export const see = (req, res) => {
-  console.log(req.params);
-  return res.send(`Watch Video #${req.params.id}`);
-};
-export const edit = (req, res) => {
-  console.log(req.params);
-  res.send("Edit Video");
-};
-export const deleteVideo = (req, res) => {
-  console.log(req.params);
-  res.send("Delete Video");
-};
+export const trending = (req, res) => res.render("home");
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
+export const deleteVideo = (req, res) => res.render("deletevideo");
 export const upload = (req, res) => res.send("Upload Video");
 export const search = (req, res) => res.send("Search");
