4ba54baf968e676d342692b1f891d8dd384c3c99
Msimusi <120442998+Msimusi@users.noreply.github.com>
2023-02-17 15:55:58 +0900

    6.23 Middlewares
    Module.pre 기능을 통해 해쉬태그를 ,로 파싱하는 기능을 배움

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -49,9 +49,7 @@ export const postEdit = async (req, res) => {
   await Video.findByIdAndUpdate(id, {
     title,
     description,
-    hashtags: hashtags
-      .split(",")
-      .map((word) => (word.startsWith("#") ? word : `#${word}`)),
+    hashtags: hashtags,
   });
 
   return res.redirect(`/videos/${id}`);
@@ -68,7 +66,7 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       description,
-      hashtags: hashtags.split(",").map((word) => `#${word}`),
+      hashtags: hashtags,
     });
     return res.redirect("/");
   } catch (error) {
