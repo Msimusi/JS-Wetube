8e788bc09853af4bfeba7f67a974d676960861ae
Msimusi <120442998+Msimusi@users.noreply.github.com>
2023-02-23 13:09:25 +0900

    11.05 Time Formatting
    Video의 전체 시간, 현재 플레이 시간 불러와서 포매팅까지 완료

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -2,13 +2,12 @@ const video = document.querySelector("video");
 
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
-const time = document.getElementById("time");
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
 const volumeRange = document.getElementById("volume");
 
 let volumeValue = 0.5;
 video.volume = volumeValue;
-
-console.log(play, mute, time, volume);
 
 const handlePlayClick = (e) => {
   // if video was playing, pause, vice versa
@@ -43,6 +42,19 @@ handleVolumeChange = (event) => {
   video.volume = value;
 };
 
+const formatTime = (seconds) =>
+  new Date(seconds * 1000).toISOString().substring(14, 19);
+
+const handleLoadedMetadata = () => {
+  return (totalTime.innerText = formatTime(Math.floor(video.duration)));
+};
+
+const handleTimeUpdate = () => {
+  return (currentTime.innerText = formatTime(Math.floor(video.currentTime)));
+};
+
 playBtn.addEventListener("click", handlePlayClick);
 mute.addEventListener("click", handleMuteClick);
 volumeRange.addEventListener("input", handleVolumeChange);
+video.addEventListener("loadedmetadata", handleLoadedMetadata);
+video.addEventListener("timeupdate", handleTimeUpdate);
