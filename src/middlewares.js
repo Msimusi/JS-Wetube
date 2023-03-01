8de805f5d300fd9f18e8993816208f44c50c2962
Msimusi <120442998+Msimusi@users.noreply.github.com>
2023-03-01 16:58:51 +0900

    Update middlewares.js

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,13 +1,13 @@
 import multer from "multer";
 import multerS3 from "multer-s3";
-import aws from "aws-sdk";
+import AWS from "aws-sdk";
 
 export const awsLogger = () => {
   AWS.config.logger = console;
   AWS.config.logger.level = "debug";
 };
 
-const s3 = new aws.S3({
+const s3 = new AWS.S3({
   credentials: {
     accessKeyId: process.env.AWS_ID,
     secretAccessKey: process.env.AWS_SECRET,
