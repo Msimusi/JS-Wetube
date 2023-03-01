532d2a34e706798ee840548ac9c12818c7328726
Msimusi <120442998+Msimusi@users.noreply.github.com>
2023-03-01 18:43:15 +0900

    프로필 / 비디오 업로드 디렉토리 분할

--- src/middlewares.js
+++ src/middlewares.js
@@ -9,9 +9,14 @@ const s3 = new aws.S3({
   },
 });
 
-const multerUploader = multerS3({
+const s3ImageUploader = multerS3({
   s3: s3,
-  bucket: "wetube-msimusi",
+  bucket: "wetube-msimusi/images",
+  acl: "public-read",
+});
+const s3VideoUploader = multerS3({
+  s3: s3,
+  bucket: "wetube-msimusi/videos",
   acl: "public-read",
 });
 
@@ -43,10 +48,10 @@ export const publicOnlyMiddleware = (req, res, next) => {
 export const avatarUpload = multer({
   dest: "uploads/avatars",
   limits: { fileSize: 10000000 },
-  storage: multerUploader,
+  storage: s3ImageUploader,
 });
 export const videoUpload = multer({
   dest: "uploads/video",
   limits: { fileSize: 100000000 },
-  storage: multerUploader,
+  storage: s3VideoUploader,
 });
