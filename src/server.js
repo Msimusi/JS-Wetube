a6204dcc9944242f6439f3e8686eed74564790ef
Msimusi <120442998+Msimusi@users.noreply.github.com>
2023-03-02 19:22:16 +0900

    Revert "비디오 녹화 변환 및 섬네일"
    This reverts commit fab67146b4c3586657b6faa9c5e94a974bdc09ea.

--- src/server.js
+++ src/server.js
@@ -15,11 +15,6 @@ const logger = morgan("dev");
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
-app.use((req, res, next) => {
-  res.header("Cross-Origin-Embedder-Policy", "require-corp");
-  res.header("Cross-Origin-Opener-Policy", "same-origin");
-  next();
-});
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
